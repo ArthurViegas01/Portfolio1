6adafdcb26c939edef841788656964ef0983bb11
Arthur Viegas <arthurpviegas@gmail.com>
2023-01-25 16:14:55 -0300

    Home component completed (Responsive)

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="homeSocial">
+        <a href="https://www.instagram.com/arthurviegaseu" className="homeSocial-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/arthur-viegas-899083205/" className="homeSocial-icon" target="_blank">
+            <i className="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/ArthurViegas01" className="homeSocial-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
