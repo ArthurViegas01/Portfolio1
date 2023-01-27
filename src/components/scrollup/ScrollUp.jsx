bb216c02a93450884c973d76f0f5a3ae718990c6
Arthur Viegas <arthurpviegas@gmail.com>
2023-01-26 23:49:16 -0300

    ScrollUp done

---
+++ src/components/scrollup/ScrollUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./scrollup.css"
+
+const ScrollUp = () => {
+    window.addEventListener("scroll", function () {
+        const scrollUp = document.querySelector(".scrollup")
+        if(this.scrollY >= 560) scrollUp.classList.add("showScroll");
+        else scrollUp.classList.remove("showScroll");
+    })
+  return (
+    <a href="#" className="scrollup">
+        <i className="uil uil-arrow-up scrollupIcon"></i>
+    </a>
+  )
+}
+
+export default ScrollUp
