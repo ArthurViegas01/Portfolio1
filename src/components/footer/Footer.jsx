c25c0eec505c6e5e5c57128df71a6390600b5736
Arthur Viegas <arthurpviegas@gmail.com>
2023-01-26 22:53:39 -0300

    Footer section completed (Responsive)

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footerContainer container">
+        <h1 className="footerTitle">Arthur Viegas</h1>
+
+        <ul className="footerList">
+          <li>
+            <a href="#about" className="footerLink">
+              Sobre mim
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footerLink" Projetos>Projetos</a>
+          </li>
+        </ul>
+
+        <div className="footerSocial">
+          <a
+            href="https://www.facebook.com/arthur.viegas.3/"
+            className="footerSocial-link"
+            target="_blank"
+          >
+            <i className="bx bxl-facebook"></i>
+          </a>
+
+          <a
+            href="https://www.instagram.com/arthurviegaseu"
+            className="footerSocial-link"
+            target="_blank"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://www.twitter.com/"
+            className="footerSocial-link"
+            target="_blank"
+          >
+            <i className="bx bxl-twitter"></i>
+          </a>
+        </div>
+
+        <span className="footerCopy">&#169; Arthur Pereira Viegas. All rigths reserved</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
